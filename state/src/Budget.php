76515526d37fcfe092e56a933ed0a54a3751af5e
thiago.henrique <theagohen001@gmail.com>
2024-01-18 16:00:15 -0300

    State porpouse

--- state/src/Budget.php
+++ state/src/Budget.php
@@ -2,9 +2,31 @@
 
 namespace Thiago\BehavioralPattern;
 
+use DomainException;
+
 class Budget
 {
     public int $itemsQuantity;
 
     public float $value;
+
+    public string $atualState;
+
+    public function applyExtraDiscount()
+    {
+        $this->value -= $this->calculateExtraValue();
+    }
+
+    public function calculateExtraValue(): float
+    {
+        if ($this->atualState == 'EM_APROVACAO') {
+            return $this->value * 0.05;
+        }
+
+        if($this->atualState == 'APROVADO') {
+            return $this->value * 0.02;
+        }
+
+        return new DomainException('Orçamentos repoados e finializados não podem receber desconto');
+    }
 }
