13822471c9c358c364e8f9d6d4fea00c04522b5c
thiago.henrique <theagohen001@gmail.com>
2024-01-19 08:07:48 -0300

    Some adjusts and add example case

--- state/src/BudgetStates/BudgetState.php
+++ state/src/BudgetStates/BudgetState.php
@@ -12,16 +12,25 @@ abstract class BudgetState
      */
     abstract public function calculateExtraValue(Budget $budget): float;
 
+    /**
+     * @throws DomainException;
+     */
     public function aprove(Budget $budget): void
     {
         throw new DomainException('Este orçamento não pode ser aprovado!');
     }
 
+    /**
+     * @throws DomainException;
+     */
     public function reprove(Budget $budget): void
     {
         throw new DomainException('Este orçamento não pode ser reprovado!');
     }
 
+    /**
+     * @throws DomainException;
+     */
     public function finish(Budget $budget): void
     {
         throw new DomainException('Este orçamento não pode ser finalizado!');
